9dceca960f271981f8291a9bc10f94be2f1b43ed
JuanHasbunZem <zemenarwow@gmail.com>
2021-02-13 16:36:29 -0800

    include isNumber function, and error div functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
+function isNumber(numInput) {
+  const isItNum = numInput;
+  if (isItNum >= 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+      
+
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    $("#heading").hide();
-    $("#forms").hide();
 
     const nameInput = $("input#userName").val();
     const numInput = $("input#userNumber").val();
-
-    $(".userName").text(nameInput);
+    if (isNumber(numInput) === true) {
+      $(".userName").text(nameInput);
     
-    $("#welcome").show();
+      $("#heading").hide();
+      $("#forms").hide();
+      $("#welcome").show();
+    } else {
+      $("#error").show();
+    }
   });
 
   $("#restart").click(function(event) {
@@ -17,5 +30,14 @@ $(document).ready(function() {
     $("#heading").show();
     $("#forms").show();
     $("#welcome").hide();
+    $("#error").hide();
+  });
+
+  $("#errorRestart").click(function(event) {
+    event.preventDefault();
+    $("#heading").show();
+    $("#forms").show();
+    $("#welcome").hide();
+    $("#error").hide();
   });
 });
